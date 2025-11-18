ffbb8a833611d2951fb79d15f2b1c57eb982a665
samarkun <samarkun4@gmail.com>
2025-11-18 10:40:29 -0500

    index.ts make export the prisma client

---
+++ packages/db/src/index.ts
@@ -0,0 +1,6 @@
+import type { PrismaClient } from "@prisma/client"
+import { PrismaClient as PrismaClientClass } from "@prisma/client"
+
+const client: PrismaClient = new PrismaClientClass();
+
+export default client;
