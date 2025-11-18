21f2a3c450adcbfe7c3303047f13586d7c31562f
samarkun <samarkun4@gmail.com>
2025-11-18 12:38:29 -0500

    apply zod validation

--- apps/http-backend/src/feature-modules/auth/auth.service.ts
+++ apps/http-backend/src/feature-modules/auth/auth.service.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { registerSchema, loginSchema , createRoomSchema } from "@repo/common/types";
 
 export const authService = {
     signUp: async(req: Request,res: Response) => {
+        const data = registerSchema.safeParse(req.body);
+        if(!data.success){
+            res.json({
+                message: "Incorrect Input"
+            })
+            return;
+        }
+
         const {userName , email , password} = req.body();
 
     },
