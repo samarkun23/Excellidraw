140e71d227f754719c76d679b81b937e368017e0
samarkun <samarkun4@gmail.com>
2025-11-18 10:44:14 -0500

    req url split and verfiy auth token

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,7 +2,13 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if(!url){
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token')
 
   ws.on('message', function message(data) {
     console.log('received: ', data)
