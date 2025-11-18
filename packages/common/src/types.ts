3a0c20cf43560b7790abbdcdd1282d3aaff13da4
samarkun <samarkun4@gmail.com>
2025-11-18 12:37:33 -0500

    types zod validation

---
+++ packages/common/src/types.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+    username: z.string().min(3, "Username should be 3 digit longer"),
+    email: z.string().email().min(8, "Email should be 8 digit longer"),
+    password: z.string().min(6, "Password must be 6 digit longer")
+})
+
+export const loginSchema = z.object({
+    username: z.string().email().optional(),
+    email: z.string().min(8).optional(),
+    password: z.string().min(6)
+})
+.refine(
+    (data) => data.username || data.email,
+    {
+        message: "Eithe email or username is required",
+        path: ["email"],
+    }
+)
+
+export const createRoomSchema = z.object({
+    name: z.string().min(3).max(20)
+})
