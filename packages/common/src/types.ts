bfff20ea386e2cce26f440ed5facacfa94ef99ec
samarkun <samarkun4@gmail.com>
2025-11-19 07:10:44 -0500

    improve the type

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
     username: z.string().min(3, "Username should be 3 digit longer"),
-    email: z.string().email().min(8, "Email should be 8 digit longer"),
+    email: z.string().email(),
     password: z.string().min(6, "Password must be 6 digit longer")
 })
 
 export const loginSchema = z.object({
-    username: z.string().email().optional(),
-    email: z.string().min(8).optional(),
+    username: z.string().min(3).optional(),
+    email: z.string().email().optional(),
     password: z.string().min(6)
 })
 .refine(
