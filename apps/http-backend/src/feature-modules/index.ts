d7b185212677efb845759553f2814938bd033d55
samarkun <samarkun4@gmail.com>
2025-11-21 01:59:10 -0500

    room route added

--- apps/http-backend/src/feature-modules/index.ts
+++ apps/http-backend/src/feature-modules/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express'
 import { authRouter } from './auth/auth.service';
+import { roomRouter } from './room/room';
 
 const mainRouter: Router = Router();
 
-mainRouter.use("/auth", authRouter)
+mainRouter.use("/auth", authRouter);
+mainRouter.use("/room", roomRouter);
 
 export { mainRouter }
