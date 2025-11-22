c3af14a3ade1621d23eb7802df64b4dfbc40ee80
samarkun <samarkun4@gmail.com>
2025-11-22 12:22:02 -0500

    base of websocket layer like a raw chat app

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,17 +1,84 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from "@repo/be-common/config"
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User {
+  ws: WebSocket,
+  rooms: string[],
+  userId: string
+}
+
+const users: User[] = []
+
+function checkToken(token: string): string | null {
+
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (typeof decoded === 'string') {
+    return null
+  }
+
+  if (!decoded || !decoded.userId) {
+    return null
+  }
+
+  return decoded.userId;
+}
+
+
 wss.on('connection', function connection(ws, request) {
   const url = request.url;
-  if(!url){
+  if (!url) {
     return;
   }
+
   const queryParams = new URLSearchParams(url.split('?')[1]);
-  const token = queryParams.get('token')
+  const token = queryParams.get('token') || "";
+  const userId = checkToken(token);
+
+  if (!userId) {
+    ws.close() 
+    return null;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws
+  })
+
 
   ws.on('message', function message(data) {
-    console.log('received: ', data)
+    const parsedData = JSON.parse(data as unknown as string);
+
+    if(parsedData.type === "join_room"){
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if(parsedData.type === "leave_room"){
+      const user = users.find(x => x.ws === ws);
+      if(!user){return}
+      user.rooms = user?.rooms.filter(x => x === parsedData.room);
+    }
+
+    if(parsedData.type === "chat"){
+      const roomId = parsedData.roomId;
+      const message = parsedData.message
+
+      users.forEach(user => {
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type: "chat",
+            message: message,
+            roomId
+          }))
+        }
+      })
+
+    }
   })
 
 })
