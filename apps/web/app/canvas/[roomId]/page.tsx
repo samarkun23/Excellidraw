01f5b45e7841ff27e43d2ec37e44ba8e037e8f87
samarkun <samarkun4@gmail.com>
2025-12-03 02:56:58 -0500

    export the http url

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -21,5 +21,9 @@ export default function Canvas() {
         <canvas ref={canvasRef} height={10000} width={10000}>
 
         </canvas>
+        <div className="absolute bottom-0 right-0">
+            <button className="bg-white text-black m-3">Rect</button>
+            <button className="bg-white text-blue-400 m-3">Circle</button>
+        </div>
     </div>
 }
