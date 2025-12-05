f9bdddcbcf225fe52e60d99bd26fe47686f096f0
samarkun <samarkun4@gmail.com>
2025-12-05 10:53:45 -0500

    here we pick the params and roomId and pass to RoomCanvas

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,29 +1,12 @@
-"use client"
-import { useEffect, useRef } from "react"
-import { initDraw } from "../../draw";
+import { RoomCanvas } from "../../../components/RoomCanvas";
 
-export default function Canvas() {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+export default async function CanvasPage({ params }:{
+    params: {
+        roomId: string
+    }
+}) {
+    const {roomId} = await params;
+    console.log(roomId);
 
-
-    useEffect(() => {
-
-        if (canvasRef.current) {
-            
-
-            initDraw(canvasRef.current);
-        }
-
-    }, [canvasRef]);
-
-
-    return <div>
-        <canvas ref={canvasRef} height={10000} width={10000}>
-
-        </canvas>
-        <div className="absolute bottom-0 right-0">
-            <button className="bg-white text-black m-3">Rect</button>
-            <button className="bg-white text-blue-400 m-3">Circle</button>
-        </div>
-    </div>
+    return <RoomCanvas roomId={roomId} />
 }
