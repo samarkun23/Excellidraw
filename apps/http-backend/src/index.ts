af2619347367d00db6038ef70269f0162e1f4afc
samarkun <samarkun4@gmail.com>
2025-12-05 10:44:41 -0500

    add a cors

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { mainRouter } from "./feature-modules/index"
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/excallidraw", mainRouter)
 
