b1c0c8261eddcd2fb7954083495253c7678b3555
samarkun <samarkun4@gmail.com>
2025-12-14 12:00:21 -0500

    Here we render Real canvas and Menu 2. we dine the state of the button and pass it to Canvas Meu frontend 3. Define state we pass to the our draw function with context roomId and socket

--- apps/web/components/Canvas.tsx
+++ apps/web/components/Canvas.tsx
@@ -1,24 +1,43 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { initDraw } from "../app/draw";
+import { CanvasMenuRec, Tool } from "./CanvasMenu";
+import { Draw } from "../app/draw/Draw";
 
-export function Canvas({roomId, socket} : {roomId : string, socket: WebSocket}) {
+export function Canvas({ roomId, socket }: { roomId: string, socket: WebSocket }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [draw , setDraw] = useState<Draw>();
+    const [selectedTool, setSelectedTool] = useState<Tool>("rect");
+
+    useEffect(() => {
+        draw?.setTool(selectedTool);
+    }, [selectedTool , draw])
+
+
     useEffect(() => {
 
         if (canvasRef.current) {
-             
-            initDraw(canvasRef.current, roomId, socket );
+            const g = new Draw(canvasRef.current , roomId , socket);
+            setDraw(g)
+            return() => {
+                g.destroy();
+            }
         }
+
 
     }, [canvasRef]);
 
-    return <div>
-        <canvas ref={canvasRef} height={10000} width={10000}>
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+
+    return <div className="relative h-[100vh] overflow-hidden">
+
+        <canvas ref={canvasRef} height={`${height}`} width={`${width}`} className="absolute top-0 left-0">
 
         </canvas>
-        <div className="absolute bottom-0 right-0">
-            <button className="bg-white text-black m-3">Rect</button>
-            <button className="bg-white text-blue-400 m-3">Circle</button>
+        <div className=" top-4 left-1/2 -translate-x-1/2 z-10 bg-gray-500 h-12 w-80 flex justify-evenly rounded-xl fixed">
+            <CanvasMenuRec selectedTool={selectedTool} setSelectedTool={setSelectedTool}/>
         </div>
+
     </div>
 }
