2e45813561b0ba79f7addc1872f5d75a2c24759d
samarkun <samarkun4@gmail.com>
2026-03-11 09:55:24 +0530

    protect the route for signin roomId is after next15 it's return the promise so we need to await or something else so we use 'use' from react 19

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,12 +1,49 @@
+'use client';
+import { useRouter } from "next/navigation";
 import { RoomCanvas } from "../../../components/RoomCanvas";
+import { use, useEffect, useState } from "react";
+import axios from "axios";
 
-export default async function CanvasPage({ params }:{
-    params: {
+export default function CanvasPage({ params }: {
+    params:Promise < {
         roomId: string
-    }
+    }>
 }) {
-    const {roomId} = await params;
-    console.log(roomId);
+    const { roomId } = use(params);
+    const router = useRouter();
+    const [user, setUser] = useState<null>(null);
+
+    useEffect(() => {
+        if (!roomId) {
+            alert("Invalid room id");
+            router.push("/signin")
+            return;
+        }
+
+        async function validateToken() {
+
+            try {
+                const res = await axios.get("http://localhost:3002/excallidraw/auth/validate", { withCredentials: true });
+                setUser(res.data.user);
+
+                if (res.status === 401) {
+                    router.push("/signin");
+                }
+            } catch (error) {
+                console.log(error);
+                router.push("/signin");
+            }
+        }
+        validateToken();
+
+
+    }, [roomId, router])
+
+    if (!user) {
+        return <div className="flex items-center justify-center h-screen w-screen bg-[#000000] text-white font-sans">
+            <div className="text-lg">Loading...</div>
+        </div>
+    }
 
     return <RoomCanvas roomId={roomId} />
 }
